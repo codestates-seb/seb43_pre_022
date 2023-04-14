a4d83734db1426ff13b346a362c3b0ce34896b89
Seohabin6078 <habin6078@gmail.com>
2023-04-14 18:55:53 +0900

    Feat: 엔티티 공동 부가 기능 추가
    advice, audit, dto, exception, response, utils, validator 패키지 추가

--- Server/src/main/java/com/codestates/Seb43Pre022Application.java
+++ Server/src/main/java/com/codestates/Seb43Pre022Application.java
@@ -1,4 +1,4 @@
-package com.codestates.seb43_pre_022;
+package com.codestates;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
