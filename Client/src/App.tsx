84e34aa9a97ab364e00394db51157a21e0f25aae
hihijin <117073214+hihijin@users.noreply.github.com>
2023-04-17 12:14:49 +0900

    [init] : eslint,prettier 설정

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,12 +1,9 @@
-import './App.css';
+import 'Global.css';
 
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <div>hello!</div>;
 }
 
 export default App;
--- Client/src/store/store.tsx
+++ Client/src/store/store.tsx
@@ -11,7 +11,7 @@ const store = configureStore({
     questionReducer,
     answerReducer,
     commentReducer,
-  }
-})
+  },
+});
 
 export default store;
