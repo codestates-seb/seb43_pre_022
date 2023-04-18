df4a64315af3a4aeb623690d231864714c225465
UseonJ <naul630@gmail.com>
2023-04-18 11:05:03 +0900

    [WIF]header 2차 작업 결과

--- Client/src/Components/Nav.tsx
+++ Client/src/Components/Nav.tsx
@@ -1,18 +1,19 @@
-import { NavDropdown } from 'react-bootstrap';
 import styled from 'styled-components';
-import Nav from 'react-bootstrap/Nav';
 import DivCom from '../Styles/DivCom';
 import ListCom from '../Styles/ListCom';
+import InputCom from '../Styles/InputCom';
 import logo from '../assets/logo.png';
 
 const NavWrapper = styled(DivCom)`
   background-color: --black-025;
+
   box-shadow: 0 1px 2px hsla(0, 0%, 0%, 0.05), 0 1px 4px hsla(0, 0%, 0%, 0.05),
     0 2px 8px hsla(0, 0%, 0%, 0.05);
+  height: 47.33px;
   width: 100vw;
 `;
 
-const StyledNav = styled(Nav)`
+const StyledNav = styled.nav`
   display: flex;
   flex-wrap: nowrap;
   justify-content: center;
@@ -24,10 +25,25 @@ const StyledNav = styled(Nav)`
   width: 90vw;
 `;
 
-const StyledListCom = styled(ListCom)`
+const NavDiv = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-right: 10px;
+  color: --header-color;
+  height: 100%;
+
+  #Navol {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const StyledListCom = styled(ListCom)`
+  padding-right: 10px;
   color: var(--header-color);
-  font-size: 10px;
+  font-size: 12px;
   &: hover {
     color: var(--header-color-hover);
     border-radius: 10px;
@@ -35,43 +51,34 @@ const StyledListCom = styled(ListCom)`
   }
 `;
 
-const NavList = styled.li`
-  color: var(--header-color);
-
-  &: hover {
-    color: var(--black-900);
-  }
-`;
-
-const NavDiv = styled.div`
-  align-items: center;
-  color: --header-color;
-`;
-
 const NavDivFlex = styled(NavDiv)`
   display: flex;
-  padding-right: 10px;
+  padding: 0 10px;
 
-  flex-basis: 400px;
-  flex: 1 10 auto;
+  flex: 1 2 auto;
 `;
 
 const NavButtonA = styled.a<{ isBlue: boolean }>`
   display: flex;
-  padding-right: 10px;
+  align-items: center;
+  padding: 0 10px;
   border-radius: 3px;
-  border-style: 0.2px;
+  cursor: pointer;
+  height: 24px;
+
   font-size: 10px;
   text-decoration: none;
-  border-color: ${(props) =>
-    props.isBlue ? 'var(--blue-button)' : 'var(--lightblue-button-text)'};
+  border: ${(props) =>
+    props.isBlue
+      ? 'var(--blue-button)'
+      : '0.2px solid var(--lightblue-button-text)'};
   color: ${(props) =>
     props.isBlue ? 'white' : 'var(--lightblue-button-text)'};
   background-color: ${(props) =>
     props.isBlue ? 'var(--blue-button)' : 'var(lightblue-button'};
 `;
 
-const NavInput = styled.input`
+const NavInput = styled(InputCom)`
   display: flex;
   flex: 10 0 auto;
 `;
@@ -79,35 +86,25 @@ const NavInput = styled.input`
 function Navi() {
   return (
     <NavWrapper>
-      <StyledNav
-        activeKey="/home"
-        onSelect={(selectedKey: EventTarget) =>
-          alert(`selected ${selectedKey}`)
-        }
-      >
-        <NavDropdown title="">
-          <div>Home</div>
-          <NavList>PUBLIC</NavList>
-          <NavList>COLLECTIVES</NavList>
-          <NavList>TEAMS</NavList>
-        </NavDropdown>
+      <StyledNav>
+        <div />
 
         <DivCom>
           <a href="/home">
-            <img src={logo} alt="" width="80px" />
+            <img src={logo} alt="" width="140px" height="40px" />
           </a>
         </DivCom>
-        <DivCom>
-          <ol>
+        <NavDiv>
+          <ol id="Navol">
             <StyledListCom>About</StyledListCom>
             <StyledListCom>Products</StyledListCom>
             <StyledListCom>For teams</StyledListCom>
           </ol>
-        </DivCom>
+        </NavDiv>
 
         <NavDivFlex>
           <img alt="" />
-          <NavInput placeholder="Search..." />
+          <NavInput placeholder="Search..." maxl="240px" />
         </NavDivFlex>
 
         <NavDiv>
