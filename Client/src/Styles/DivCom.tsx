fd2be998979b9f5d09a20a56fa0abecb32d636dd
UseonJ <naul630@gmail.com>
2023-04-18 11:05:51 +0900

    [Design]공통사용 엘리먼트 div,li

---
+++ Client/src/Styles/DivCom.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const DivCom = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font: unset;
+`;
+
+export default DivCom;
