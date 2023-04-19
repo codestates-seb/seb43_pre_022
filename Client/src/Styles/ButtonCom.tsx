e679013d66c52bb627f4329a59139c9340bb3783
UseonJ <naul630@gmail.com>
2023-04-19 22:47:13 +0900

    [feat]버튼 엘리먼트 업데이트

--- Client/src/Styles/ButtonCom.tsx
+++ Client/src/Styles/ButtonCom.tsx
@@ -3,12 +3,16 @@ import styled from 'styled-components';
 interface Buttonprops {
   paddings?: string;
   radius?: string;
+  bgcolor?: string;
+  color?: string;
 }
 
 const ButtonCom = styled.button<Buttonprops>`
   border-style: none;
   border-radius: ${(props) => props.radius || '3px'};
   padding: ${(props) => props.paddings};
+  background-color: ${(props) => props.bgcolor || 'transparent'};
+  color: ${(props) => props.color || 'black'};
 `;
 
 export default ButtonCom;
