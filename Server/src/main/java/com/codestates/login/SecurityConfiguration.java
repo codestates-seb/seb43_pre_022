1aa029a5b0a8933f44b71f410df4a6f04e1cf56a
kkihoon2 <mygreensky7@naver.com>
2023-04-19 11:54:03 +0900

    fix : login url 수정 API명세서에 따르지 않았던 login쪽 url API명세서에 따라 수정

--- Server/src/main/java/com/codestates/login/SecurityConfiguration.java
+++ Server/src/main/java/com/codestates/login/SecurityConfiguration.java
@@ -77,7 +77,7 @@ public class SecurityConfiguration {
             AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
 
             JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager,jwtTokenizer);
-            jwtAuthenticationFilter.setFilterProcessesUrl("/login");
+            jwtAuthenticationFilter.setFilterProcessesUrl("/api/auths/login");
             jwtAuthenticationFilter.setAuthenticationSuccessHandler(new MemberAuthenticationSuccessHandler());
             // 로그인 성공시 핸들러
             jwtAuthenticationFilter.setAuthenticationFailureHandler(new MemberAuthenticationFailureHandler());
