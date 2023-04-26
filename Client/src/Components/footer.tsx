0612d91f6d8354859a65293276b90c998ab84dc6
UseonJ <naul630@gmail.com>
2023-04-26 10:21:39 +0900

    Update footer.tsx

--- Client/src/Components/footer.tsx
+++ Client/src/Components/footer.tsx
@@ -68,9 +68,6 @@ function Footer() {
   const linkedinLink = 'https://www.linkedin.com/company/stack-overflow';
   const igLink = 'https://www.instagram.com/thestackoverflow/';
 
-  if (window.location.pathname === '/signup') return null;
-  if (window.location.pathname === '/signin') return null;
-
   return (
     <FooterContainer>
       <FooterInfoContainer>
@@ -189,3 +186,4 @@ function Footer() {
 }
 
 export default Footer;
+
