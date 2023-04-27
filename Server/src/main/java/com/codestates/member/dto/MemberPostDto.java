adbd46a22b938fb4bdd678847e91c72925e8fbdb
Seohabin6078 <habin6078@gmail.com>
2023-04-27 12:17:37 +0900

    fix: 배포전 최종3

--- Server/src/main/java/com/codestates/member/dto/MemberPostDto.java
+++ Server/src/main/java/com/codestates/member/dto/MemberPostDto.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @AllArgsConstructor
@@ -13,6 +14,7 @@ import javax.validation.constraints.NotBlank;
 
 public class MemberPostDto {
     @NotBlank(message = "이름은 공백이 아니어야 합니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "이름은 영문자와 숫자로만 이뤄져야 합니다.")
     private String displayName;
     @NotBlank
     @Email
